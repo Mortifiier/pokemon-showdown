3c7c175c9489ceaa9b99bd01b61caf1b404b9b39
Mortifiier <74686542+Mortifiier@users.noreply.github.com>
2020-11-21 13:12:36 -0600

    Update pokedex.ts

--- data/mods/test/pokedex.ts
+++ data/mods/test/pokedex.ts
@@ -1,6 +1,5 @@
 export const Pokedex: {[k: string]: ModdedSpeciesData} = {
-	amoonguss: {
-		inherit: true,
-		baseStats: {hp: 114, atk: 85, def: 170, spa: 185, spd: 80, spe: 30},
+	blissey: {
+		baseStats: {hp: 2, atk: 85, def: 170, spa: 185, spd: 80, spe: 30},
 	},
 };
